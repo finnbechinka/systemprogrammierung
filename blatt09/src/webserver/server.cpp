e759e26a9649bfe739ba611c89291faf9cbda5c4
Finn Bechinka <finn.bechinka@fh-bielefeld.de>
2022-01-10 10:39:47 +0100

    daemon funktioniert; signale noch nicht getestet

--- blatt09/src/webserver/server.cpp
+++ blatt09/src/webserver/server.cpp
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <syslog.h>
 
 #include <vector>
 #include <sstream>
@@ -19,8 +20,6 @@
 
 using namespace std;
 
-#define DEBUG(x)    cout << "Debug: " << x << endl
-#define PANIC(msg)  {perror(msg); abort();}
 #define MAXBUF      1024
 #define MYPORT      15151
 
@@ -28,6 +27,8 @@ string buildHeader(size_t len, string ext);
 string readFile(FILE* file);
 int mysend(int fd, const char *buf, size_t n, int flags);
 int startWebserver(int argc, char* argv[]);
+void sighupHandler(int sig);
+void sigtermHandler(int sig);
 
 
 int main(int argc, char* argv[]) {
@@ -44,8 +45,26 @@ int main(int argc, char* argv[]) {
             // error beim versuch neue session zu erstellen
             exit(EXIT_FAILURE);
         }
+
+        // SIGCHLD und SIGHUP ignorieren
+        signal(SIGCHLD, SIG_IGN);
+        signal(SIGHUP, SIG_IGN);
+
+        p = fork();
+
+        if(p < 0){
+            //error
+            exit(EXIT_FAILURE);
+        }
+
+        if(p > 0){
+            //kill parent
+            exit(EXIT_SUCCESS);
+        }
+        //als child(jetzt daemon) fortfahren
+
+        umask(0);
         //waechseln in anderes arbeitsverzeichnis: im moment nicht gewollt
-        //umask(0);
         //chdir("");
 
         //geerbte filedeskriptoren schliessen
@@ -57,26 +76,29 @@ int main(int argc, char* argv[]) {
         int status = open("/dev/null", O_EXCL); /* stdin,  0 */
         if(status == EEXIST){
             //eine daemon instanz lauft schon
-            return EXIT_FAILURE
-        }         
+            return EXIT_FAILURE;
+        }
         dup2(STDIN_FILENO, STDOUT_FILENO); /* stdout, 1 */
         dup2(STDIN_FILENO, STDERR_FILENO); /* stderr, 2 */
+
+        openlog("Temperatur-Daemon", LOG_PID, LOG_USER);
+        syslog(LOG_INFO, "Daemon gestartet");
 
         //SIGINT UND SIGWINCH ingnorieren, empfohlen in vorlesung
         signal(SIGINT, SIG_IGN);
         signal(SIGWINCH, SIG_IGN);
 
-        //alle anderen auf default setzen
-        signal(SIGCHLD, SIG_DFL);
-        signal(SIGSTOP, SIG_DFL);
-        signal(SIGSEGV, SIG_DFL);
-        signal(SIGHUP, SIG_DFL);
-        signal(SIGTERM, SIG_DFL);
-        signal(SIGKILL, SIG_DFL);
-        signal(SIGALRM, SIG_DFL);
-
         //signalmaske löschen
-        //sigprocmask();
+        // sigprocmask();
+
+        if(signal(SIGHUP, sighupHandler) == SIG_ERR){
+            syslog(LOG_INFO, "sighuphandler zuweisung sigerr");
+        }
+
+        if(signal(SIGTERM, sigtermHandler) == SIG_ERR){
+            syslog(LOG_INFO, "sigtermhandler zuweisung sigerr");
+        }
+
 
         //prozess ist nun deamon: eigentliche funktionalitaet starten
         startWebserver(argc, argv);
@@ -144,12 +166,26 @@ int mysend(int fd, const char *buf, size_t n, int flags){
     while(n > 0){
         bytesWritten = send(fd, buf, n, flags);
         if(bytesWritten == -1){
+            syslog(LOG_INFO, "fehler beim send call");
             return -1;
         }
         n -= bytesWritten;
         buf += bytesWritten;
     }
     return 0;
+}
+
+void sighupHandler(int sig){
+    if(sig == SIGHUP){
+        syslog(LOG_INFO, "sighuphandler called: nothing yet");
+    }
+}
+
+void sigtermHandler(int sig){
+    if(sig == SIGTERM){
+        syslog(LOG_INFO, "sigtermhandler called: daemon wird beendet");
+        exit(EXIT_SUCCESS);
+    }
 }
 
 int startWebserver(int argc, char* argv[]){
@@ -160,6 +196,7 @@ int startWebserver(int argc, char* argv[]){
         port = stoi(string(argv[2]));
     }else{
         // falsche argumente
+        syslog(LOG_INFO, "argumentanzahl falsch");
         return EXIT_FAILURE;
     }
 
@@ -176,7 +213,7 @@ int startWebserver(int argc, char* argv[]){
     memset(&srv, 0, sizeof(srv));
     srv.sin_family = AF_INET;
     srv.sin_addr.s_addr = htonl(INADDR_LOOPBACK);  // 127.0.0.1
-    srv.sin_port = htons(port);                  // Port 15151
+    srv.sin_port = htons(port);
 
     // Socket an Namen binden
     bind(fd, (const struct sockaddr *) &srv, sizeof(srv));
@@ -191,12 +228,14 @@ int startWebserver(int argc, char* argv[]){
     for (;;) { // Server-Schleife
         // Verbindung annehmen und blockieren, bis Verbindung
         in_fd = accept(fd, NULL, NULL); // Adresse vom Komm.-Partner interessiert mich nicht
+        syslog(LOG_INFO, "verbindung aufgenommen");
 
         for (;;) { // Kommunikation mit Client
             memset(buf, 0, MAXBUF);
             n = -1;
             n = recv(in_fd, buf, MAXBUF, 0);
             if (n == 0) {
+                syslog(LOG_INFO, "verbindung geschlossen");
                 close(in_fd);  // nur den Client-Socket schliessen
                 break; // raus aus der Komm.-Schleife mit Client, naechste Verbindung akzeptieren
             }
@@ -224,24 +263,29 @@ int startWebserver(int argc, char* argv[]){
             string msg = "req error";
             //gucken ob get request
             if(parsedFirstLine.at(0) == "GET"){
+                syslog(LOG_INFO, "GET request empfangen");
                 string filepath;
                 filepath += path;
                 filepath += parsedFirstLine.at(1);
 
                 if (FILE *file = fopen(filepath.c_str(), "r")) {
+                    syslog(LOG_INFO, "angefragte Datei gefunden");
                     string content = readFile(file);
                     string ext = filepath.substr(filepath.find_last_of('.'), filepath.length());
                     msg.clear();
                     msg += buildHeader(content.length(), ext);
                     msg += content;
                     mysend(in_fd, msg.c_str(), msg.length(), 0);
+                    syslog(LOG_INFO, "angefragte Datei ausgeliefert");
                     fclose(file);
                 } else {
+                    syslog(LOG_INFO, "angefragte Datei nicht gefunden");
                     msg.clear();
                     msg += "Fehler 404 (Not Found)";
                     mysend(in_fd, msg.c_str(), msg.length(), 0);
                 } 
             }else{
+                // echo
                 msg.clear();
                 msg = (string) buf;
                 mysend(in_fd, msg.c_str(), msg.length(), 0);
